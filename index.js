aef744e546e299353bfc77a4e5b709f65840a028
Max S <itsmaxthedev@protonmail.com>
2022-10-17 12:38:04 -0400

    stuck on figuring out what the api returned for the current query

--- index.js
+++ index.js
@@ -40,35 +40,47 @@ function isolateInnerHTMLFieldsFromTitles(titlesObject) { // titles is an HTMLCo
       i++ //iterator
   }
   console.log(titlesArray);
-
+  return titlesArray;
 }
 
 
 
 //Run functions
 
-isolateInnerHTMLFieldsFromTitles(getTitlesObject());
+var firstTitle = isolateInnerHTMLFieldsFromTitles(getTitlesObject())[0];
+console.log(firstTitle);
+console.log("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed"+"&term="+firstTitle);
+var titlesArrayInput = isolateInnerHTMLFieldsFromTitles(getTitlesObject());
 
 function getPMIDsFromAPI (inputArray) {
   const xhr = new XMLHttpRequest();
   var pmids;
 
   xhr.onload = () => {
-    console.log(xhr.responseXML.documentElement.nodeName); // document can be parsed here
-    pmids = xhr.responseXML.getElementsByTagName("PMID") // titles is an HTMLCollection object
-    //console.log(titles);
-    //still need to remove the actual tags from the list items (should leave only )
-    console.log(pmids[0])
+    pmids = xhr.responseXML;
+    console.log(xhr.responseXML.getElementsByTagName("PMID")[0]);
+    console.log(pmids[0]); // check first pmid to see if results were correct
   }
 
   xhr.onerror = () => {
     console.log("Error while getting XML.");
   }
 
-  xhr.open("GET", "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed",false); // set async to false to allow response.xml to be accessed outside of onload()
-    xhr.send();
+  // press ctrl+k+c to comment something out in vscode
+  // var i = 0, len = titlesObject.length;
+  // var titlesArray = [];
+  console.log("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed"+"term"+firstTitle);
+  // while (i < len) {
+      // your code
+  xhr.open("GET", "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed"+"&term="+firstTitle,false); // set async to false to allow response.xml to be accessed outside of onload()
+  xhr.send();
+  //    i++ //iterator
+  // }
+
 
   //console.log(titles[7]);
 
   return pmids;
-} 
+}
+
+getPMIDsFromAPI(titlesArrayInput);
