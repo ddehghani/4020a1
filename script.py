3bc3eff0055655ad6c26f9c7254173da9e0a633b
ddehghani <ddehghani@icloud.com>
2022-10-19 01:12:20 -0400

    final changes for tonight

--- script.py
+++ script.py
@@ -12,30 +12,41 @@ class Article():
 def main():
     PATH_TO_SOURCE_XML = './4020a1-datasets.xml'
     ESEARCH_BASE_URL = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi'
-    articles = extractArticles(PATH_TO_SOURCE_XML)
-    print(f'{len(articles) = }')
+    PATH_TO_OUTPUT_FILE = 'groupID_result.xml'
+    articles = extractArticles(PATH_TO_SOURCE_XML) [0:10]
     ids = getIdsByAPICall(articles,ESEARCH_BASE_URL)
-    print(f'{len(ids) = }')
-    print(ids)
+    assert len(ids) == len(articles) , "some articles could not be found"
+    createXMLfromIds(PATH_TO_OUTPUT_FILE,ids, articles)
+
+def createXMLfromIds(outfile: str, ids: list[int], articles: list[Article]) -> None:
+    article_set = ET.Element('PubmedArticleset')
+    for art, id in zip(articles, ids):
+        article = ET.SubElement(article_set,'PubmedArticle')
+        pmid = ET.SubElement(article,'PMID')
+        pmid.text = str(id)
+        articleTitle = ET.SubElement(article,'ArticleTitle')
+        articleTitle.text = art.ArticleTitle
+    with open(outfile, 'wt') as file:
+        file.write(ET.tostring(article_set, encoding='utf8', method='xml').decode('utf8'))
 
 def getIdsByAPICall(articles: list[Article], baseurl) -> list[int]:
     result = []
     failCount = 0
-    for article in articles[:]:
+    for article in articles:
         params = {'db': 'pubmed', 'term': article.ArticleTitle, 'field': 'title'}
         try:
             root = ET.fromstring(requests.get(baseurl, params=params).text)
             result.append(int(root.find('IdList')[0].text))
             resultCount = root.find('Count').text
             if resultCount != '1':
-                #print(f'{article.ArticleTitle = }')
-                #print(f'{resultCount = }')
+                # print(f'{article.ArticleTitle = }')
+                # print(f'{resultCount = }')
                 failCount += 1
         except Exception as e:
             print(e)
             failCount += 1
         time.sleep(0.2) #api call limit rate
-    print(f'{len(result) * 100 / (len(result) + failCount)}:.2f%')
+    # print(f'{len(result) * 100 / (len(result) + failCount)}:.2f%')
     return result
 
 def extractArticles(sourceFile: str) -> list[Article]:
